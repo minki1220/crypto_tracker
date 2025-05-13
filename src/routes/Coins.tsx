8adcd6074fffa5960c869bff75c7f1c8cbf553cc
minki1220 <130380152+minki1220@users.noreply.github.com>
2025-05-14 00:44:40 +0900

    coins fetch api

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,10 +1,95 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  margin-bottom: 10px;
+  border-radius: 15px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
 
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
+
+const Loading = styled.div`
+  text-align: center;
+  padding: 40px;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 function Coins() {
-  return <Title>coins</Title>;
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, isLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await (await response).json();
+      setCoins(json.slice(0, 100));
+      isLoading(false);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>coins</Title>
+      </Header>
+      {loading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`${coin.id}`}>
+                <Img
+                  src={`https://cryptoicon-api.pages.dev/icons/128/color/${coin.symbol.toLowerCase()}.png`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
 
 export default Coins;
